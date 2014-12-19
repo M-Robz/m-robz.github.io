a18916b96aff9ea60c7e8696ac6d1c169daae283
M-Robz <axman97@hotmail.com>
2014-12-18 22:17:37 -0500

    main js, css

---
+++ script.js
@@ -0,0 +1,13 @@
+function viewLogos() {
+	var logoDiv = document.getElementById("logo-box");
+	if(logoDiv.style.visibility == "visible") {
+		logoDiv.style.width = "0";
+		setTimeout( function() {
+			logoDiv.style.visibility = "hidden";
+		}, 1000);
+	}
+	else {
+		logoDiv.style.visibility = "visible";
+		logoDiv.style.width = "440px";
+	}
+}
