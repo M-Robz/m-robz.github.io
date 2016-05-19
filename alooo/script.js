94098622f303e998071f6f9c265268e406300f7a
M-Robz <axman97@hotmail.com>
2016-05-19 13:04:17 -0500

    alooo script: menu clickout

--- alooo/script.js
+++ alooo/script.js
@@ -1,21 +1,49 @@
 /* ------------- Open and close navigation menu items ------------- */
 
-var lastMenu = 0;
+var lastMenu; // the most recently opened subNav menu
 
+// open or close subNav menu when main navItem clicked
 function toggleMenu(menuID) {
 	newMenu = document.getElementById(menuID);
+	
+	// show newMenu if hidden
 	if (newMenu.style.visibility == 'hidden') {
+		
+		// hide lastMenu if defined
 		if (lastMenu) lastMenu.style.visibility = 'hidden';
+		
+		// make newMenu visible and assign it to lastMenu
 		newMenu.style.visibility = 'visible';
 		lastMenu = newMenu;
+		
+		// add listener for click on body
+		document.body.addEventListener('click', menuClickOut);
+		
 	} else {
+		
+		// hide newMenu if visible
 		newMenu.style.visibility = 'hidden';
+		
+		// remove listener
+		document.body.removeEventListener('click', menuClickOut);
+		
 	}
 }
 
+// close subNav menu on mouse out
 function closeMenu(menuID) {
 	document.getElementById(menuID).style.visibility = 'hidden';
+	document.body.removeEventListener('click', menuClickOut);
 }
+
+// close subNav menu if document clicked outside nav menu
+function menuClickOut(event) {
+	if (event.target.parentNode.className != "navItem" && event.target.className != "subNav") {
+		lastMenu.style.visibility = 'hidden';
+		document.body.removeEventListener('click', menuClickOut);
+	}
+}
+
 
 /* ------------- Switch tabs ------------- */
 
