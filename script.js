d506631ce1029374ee77c8aa917fe533be9ba164
M-Robz <axman97@hotmail.com>
2016-05-31 19:38:42 -0500

    Main script: logo box clickout

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-function viewLogos() {
+function showLogos() {
 	var logoDiv = document.getElementById("logo-box");
-	if(logoDiv.style.visibility == "visible") {
+	if (logoDiv.style.visibility == "hidden") {
+		logoDiv.style.visibility = "visible";
+		logoDiv.style.width = "660px";
+		document.addEventListener('click', logoClickOut);
+	}
+}
+
+function hideLogos() {
+	var logoDiv = document.getElementById("logo-box");
+	if (logoDiv.style.visibility == "visible") {
 		logoDiv.style.width = "0";
 		setTimeout( function() {
 			logoDiv.style.visibility = "hidden";
-		}, 1000);
+		}, 500);
+		document.removeEventListener('click', logoClickOut);
 	}
-	else {
-		logoDiv.style.visibility = "visible";
-		logoDiv.style.width = "660px";
+}
+
+function logoClickOut(event) {
+	if (event.target.id != "logo-box" && event.target.parentNode.id != "logo-box" && event.target.parentNode.id != "logos") {
+		hideLogos();
 	}
 }
