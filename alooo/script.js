f1c06e6e544f67c3ec35b8b29851c2f4d1d92da5
M-Robz <axman97@hotmail.com>
2016-06-17 12:45:54 -0500

    add alooo mobile version; remove rhl site

--- alooo/script.js
+++ alooo/script.js
@@ -1,46 +1,70 @@
 /* ------------- Open and close navigation menu items ------------- */
 
-var lastMenu; // the most recently opened subNav menu
+// COMPATIBILITY: dummy onclick property of #pageContainer permits event delegation to non-interactive elements in Safari Mobile 7.0+ (https://developer.mozilla.org/en-US/docs/Web/Events/click)
+
+var lastSubNav; // the most recently opened subNav menu
 
 // open or close subNav menu when main navItem clicked
-function toggleMenu(menuID) {
-	newMenu = document.getElementById(menuID);
+function toggleSubNav(menuID) {
+	var newSubNav = document.getElementById(menuID);
 	
-	// show newMenu if hidden
-	if (newMenu.style.visibility == 'hidden') {
+	if (newSubNav.style.visibility == 'hidden') {
+	// show newSubNav if hidden
+	
+		// hide lastSubNav if defined
+		if (lastSubNav) lastSubNav.style.visibility = 'hidden';
 		
-		// hide lastMenu if defined
-		if (lastMenu) lastMenu.style.visibility = 'hidden';
+		// make newSubNav visible
+		newSubNav.style.visibility = 'visible';
 		
-		// make newMenu visible and assign it to lastMenu
-		newMenu.style.visibility = 'visible';
-		lastMenu = newMenu;
-		
-		// add listener for click on body
-		document.body.addEventListener('click', menuClickOut);
+		// add listener for click on pageContainer
+		document.getElementById('pageContainer').addEventListener('click', menuClickOut);
 		
 	} else {
+	// hide newSubNav if visible
 		
-		// hide newMenu if visible
-		newMenu.style.visibility = 'hidden';
-		
-		// remove listener
-		document.body.removeEventListener('click', menuClickOut);
+		newSubNav.style.visibility = 'hidden';
 		
 	}
+	
+	lastSubNav = newSubNav;
 }
 
 // close subNav menu on mouse out
-function closeMenu(menuID) {
+function closeSubNav(menuID) {
 	document.getElementById(menuID).style.visibility = 'hidden';
-	document.body.removeEventListener('click', menuClickOut);
 }
 
-// close subNav menu if document clicked outside nav menu
+// if document clicked outside main menu, close subNav and hide main menu in mobile version
 function menuClickOut(event) {
-	if (event.target.parentNode.className != "navItem" && event.target.className != "subNav") {
-		lastMenu.style.visibility = 'hidden';
-		document.body.removeEventListener('click', menuClickOut);
+	var mainNav = document.getElementsByTagName('NAV')[0];
+	
+	if (event.target != mainNav && event.target.parentNode.className != 'menuButton' && event.target.parentNode.parentNode.className != 'menuButton' && event.target.parentNode.className != 'navItem' && event.target.className != 'subNav') {
+		
+		// remove listener, hide lastSubNav if defined, and hide main menu in mobile version
+		document.getElementById('pageContainer').removeEventListener('click', menuClickOut);
+		if (lastSubNav) lastSubNav.style.visibility = 'hidden';
+		mainNav.className = '';
+	}
+}
+
+// show or hide main menu in mobile version
+function toggleMainNavVis() {
+	var mainNav = document.getElementsByTagName('NAV')[0];
+	
+	if (mainNav.className != 'visible') {
+	//show menu if hidden and add listener
+	
+		mainNav.className = 'visible';
+		document.getElementById('pageContainer').addEventListener('click', menuClickOut);
+		
+	} else {
+	// hide menu and subNav if open, and remove listener
+		
+		mainNav.className = '';
+		lastSubNav.style.visibility = 'hidden';
+		document.getElementById('pageContainer').removeEventListener('click', menuClickOut);
+		
 	}
 }
 
@@ -48,7 +72,7 @@ function menuClickOut(event) {
 /* ------------- Switch tabs ------------- */
 
 function switchTab(tabID) {
-	var divArray = document.getElementsByTagName('div');
+	var divArray = document.getElementsByTagName('DIV');
 	for (i=0; i<divArray.length; i++) {
 		if (divArray[i].id == tabID) {
 			
@@ -59,7 +83,7 @@ function switchTab(tabID) {
 		else {
 			
 			// remove class "visible" from old tab
-			posVisible = divArray[i].className.indexOf(' visible');
+			var posVisible = divArray[i].className.indexOf(' visible');
 			if (posVisible > -1) {
 				divArray[i].className = divArray[i].className.substring(0, posVisible);
 			}
--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 function showLogos() {
-	var logoDiv = document.getElementById("logo-box");
-	if (logoDiv.style.visibility == "hidden") {
-		logoDiv.style.visibility = "visible";
-		logoDiv.style.width = "660px";
+	var logoDiv = document.getElementById('logo-box');
+	if (logoDiv.style.visibility == 'hidden') {
+		logoDiv.style.visibility = 'visible';
+		logoDiv.className = 'hasWidth';
 		document.addEventListener('click', logoClickOut);
 	}
 }
 
 function hideLogos() {
-	var logoDiv = document.getElementById("logo-box");
-	if (logoDiv.style.visibility == "visible") {
-		logoDiv.style.width = "0";
-		setTimeout( function() {
-			logoDiv.style.visibility = "hidden";
-		}, 500);
+	var logoDiv = document.getElementById('logo-box');
+	if (logoDiv.style.visibility == 'visible') {
 		document.removeEventListener('click', logoClickOut);
+		logoDiv.className = '';
+		setTimeout( function() {
+			logoDiv.style.visibility = 'hidden';
+		}, 500);
 	}
 }
 
 function logoClickOut(event) {
-	if (event.target.id != "logo-box" && event.target.parentNode.id != "logo-box" && event.target.parentNode.id != "logos") {
+	if (event.target.id != 'logo-box' && event.target.parentNode.id != 'logo-box' && event.target.parentNode.id != 'logos') {
 		hideLogos();
 	}
 }
